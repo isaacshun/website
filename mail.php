8b38fb0015eb8432d373302edd0ae3e1d6c18551
Isaac Tsang <isaacshun@yahoo.com>
2021-01-02 11:07:59 -0500

    commit

--- mail.php
+++ mail.php
@@ -1,22 +1,15 @@
 <?php
-  $fname = $_POST['fname'];
-  $lname = $_POST['lname'];
-  $visitor_email = $_POST['email'];
+
+if (isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  $email_from = "luttleboi@gmail.com";
-  $email_subject = "New Form Submission";
+  $mailTo = "isaacshun@yahoo.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-  $email_body = "User Name: $fname $lname.\n". 
-                  "User Email: $visitor_email.\n". 
-                    "Message: $message.\n";
-
-  $to = "isaacshun@yahoo.com";
-
-  $headers = "From $email_from \r\n";
-  $headers .= "Reply-To: $visitor_email \r\n";
-
-  mail($to, $email_subject, $email_body, $headers);
-
-  headers("Location: index.html");
-?>
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.htm?mailsend");
+}
